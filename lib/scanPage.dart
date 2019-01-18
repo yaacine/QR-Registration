151702a626c05d971957c0798ed461525284e738
KERFAH Issam <ikerfah@gmail.com>
2019-01-18 18:46:50 +0100

    Add Auth API

--- lib/scanPage.dart
+++ lib/scanPage.dart
@@ -4,6 +4,7 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ScanPage extends StatefulWidget {
   @override
@@ -11,14 +12,36 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
+  String barcode = "";
+  bool tokenTaken = false;
+  // auth
 
-  String barcode ="";
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+  getToken() {
+    _googleSignIn.signIn().then((result) {
+      result.authentication.then((googleKey) {
+        tokenTaken = googleKey.accessToken;
+        print(googleKey.accessToken);
+        print(_googleSignIn.currentUser.displayName);
+      }).catchError((err) {
+        print('inner error');
+      });
+    }).catchError((err) {
+      print('error occured');
+    });
+  }
+
   @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-    }
-  
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,40 +59,62 @@ class _ScanPageState extends State<ScanPage> {
                     color: Colors.blue,
                     textColor: Colors.white,
                     splashColor: Colors.blueGrey,
-                    onPressed: scan,
-                    child: const Text('START CAMERA SCAN')
-                ),
-              )
-              ,
+                    onPressed: getToken,
+                    child: const Text('Get Google Token')),
+              ),
+              Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    "Got Token ? "+tokenTaken.toString(),
+                    textAlign: TextAlign.center,
+                  )),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text(barcode, textAlign: TextAlign.center,),
-              )
-              ,
+                child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    splashColor: Colors.blueGrey,
+                    onPressed: scan,
+                    child: const Text('START CAMERA SCAN')),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(
+                  barcode,
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ));
   }
 
-
-Future scan() async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      setState(() => this.barcode = barcode);
-    } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
-    }  on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          this.barcode = 'The user did not grant the camera permission!';
-        });
-      } else {
+  Future scan() async {
+    if(!tokenTaken){
+      setState(() {
+        this.barcode = 'You dont have acces token';
+      });
+    }else{
+      try {
+        String barcode = await BarcodeScanner.scan();
+        setState(() => this.barcode = barcode);
+      } on FormatException {
+        setState(() => this.barcode =
+        'null (User returned using the "back"-button before scanning anything. Result)');
+      } on PlatformException catch (e) {
+        if (e.code == BarcodeScanner.CameraAccessDenied) {
+          setState(() {
+            this.barcode = 'The user did not grant the camera permission!';
+          });
+        } else {
+          setState(() => this.barcode = 'Unknown error: $e');
+        }
+      } catch (e) {
         setState(() => this.barcode = 'Unknown error: $e');
       }
     }
-    catch (e) {
-      setState(() => this.barcode = 'Unknown error: $e');
-    }
-  }
 
+
+  }
 }
